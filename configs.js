5e23ecd593e15d98baca51a7e1383763dc735db9
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-04-09 10:56:13 +0530

    create config file

---
+++ configs.js
@@ -0,0 +1,8 @@
+// This file contains all the configurations
+
+var config = {
+    mainDB: "mongodb://localhost:27017/test",
+    subscriptionsDB: "mongodb://localhost:27017/subscriptions"
+};
+
+module.exports = { config };
