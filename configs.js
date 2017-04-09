310dfac6a5fce93066d7ddb67fbb1d2826c7de8c
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-04-09 22:54:39 +0530

    update configs to make notifier independent

--- configs.js
+++ configs.js
@@ -1,8 +1,19 @@
 // This file contains all the configurations
+/* peopleDB :        the URL of main database
+ * subscriptionsDB:  the url of subscritions database
+ * peopleColl:       the name of collection of users
+ * subscribersColl:  the name of collection of subscribers
+ * peopleNS:         namespace of people collection
+ * subscribersNS:    namespace of people collection
+ */
 
 var config = {
-    mainDB: "mongodb://localhost:27017/test",
-    subscriptionsDB: "mongodb://localhost:27017/subscriptions"
+    peopleDB: "mongodb://localhost:27017/test",
+    peopleColl: "people",
+    peopleNS: "test.people",
+    subscriptionsDB: "mongodb://localhost:27017/test",
+    subscribersNS: "test.subscribers",
+    subscribersColl: "subscribers"
 };
 
 module.exports = { config };
